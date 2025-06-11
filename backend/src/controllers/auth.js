d96c336dd8931d479b56d429cef8c7ff44e5b1a1
gedenoss <geoffreybougueroua@gmail.com>
2025-06-11 10:19:24 +0200

    front2

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,6 +1,6 @@
-import db from '../db.js'
-import validator from 'validator';
-import { OAuth2Client } from 'google-auth-library';
+import db from "../db.js";
+import validator from "validator";
+import { OAuth2Client } from "google-auth-library";
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
@@ -34,27 +34,34 @@ export async function signUp(req, reply) {
   let { login, password, email } = req.body;
 
   if (!login || !password || !email) {
-    return reply.status(400).send({ error: 'Login, password and email are required' });
+    return reply
+      .status(400)
+      .send({ error: "Login, password and email are required" });
   }
 
   login = login.trim();
   email = email.trim();
 
-  if (await loginExist(login) || await emailExist(email)) {
-    return reply.status(400).send({ error: 'User with this login or email already exists' });
+  if ((await loginExist(login)) || (await emailExist(email))) {
+    return reply
+      .status(400)
+      .send({ error: "User with this login or email already exists" });
   }
 
   if (login.length > 20 || login.length < 2) {
-    return reply.status(400).send({ error: 'Login must be between 2 and 20 characters long' });
+    return reply
+      .status(400)
+      .send({ error: "Login must be between 2 and 20 characters long" });
   }
 
   if (!validator.isEmail(email)) {
-    return reply.status(400).send({ error: 'Invalid email format' });
+    return reply.status(400).send({ error: "Invalid email format" });
   }
 
   if (!isStrongPassword(password)) {
     return reply.status(400).send({
-      error: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+      error:
+        "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
     });
   }
 
@@ -62,7 +69,9 @@ export async function signUp(req, reply) {
 
   const avatarUrl = `https://api.dicebear.com/9.x/bottts-neutral/svg?seed=${encodeURIComponent(login)}`;
   try {
-    const stmt = db.prepare('INSERT INTO users (login, password, email, avatarUrl) VALUES (?, ?, ?, ?)');
+    const stmt = db.prepare(
+      "INSERT INTO users (login, password, email, avatarUrl) VALUES (?, ?, ?, ?)",
+    );
     stmt.run(login, password, email, avatarUrl);
     const token = await reply.jwtSign({ login, email, avatarUrl });
     reply.code(201).send({ token });
@@ -73,7 +82,7 @@ export async function signUp(req, reply) {
 
 export async function signUpGoogle(req, reply) {
   const token = req.body.token || req.body.idtoken;
-  console.log('Google sign-up token:', token);
+  console.log("Google sign-up token:", token);
   try {
     const ticket = await client.verifyIdToken({
       idToken: token,
@@ -84,27 +93,33 @@ export async function signUpGoogle(req, reply) {
     const { email, name } = payload;
 
     if (!email || !name) {
-      return reply.status(400).send({ error: 'Invalid Google token' });
+      return reply.status(400).send({ error: "Invalid Google token" });
     }
 
     if (await emailExist(email)) {
-      return reply.status(400).send({ error: 'User with this email already exists' });
+      return reply
+        .status(400)
+        .send({ error: "User with this email already exists" });
     }
 
-    const login = name.replace(/\s+/g, '').toLowerCase();
+    const login = name.replace(/\s+/g, "").toLowerCase();
     if (await loginExist(login)) {
-      return reply.status(400).send({ error: 'User with this login already exists' });
+      return reply
+        .status(400)
+        .send({ error: "User with this login already exists" });
     }
-    console.log('Google sign-up for:', login, email);
+    console.log("Google sign-up for:", login, email);
     const avatarUrl = `https://api.dicebear.com/9.x/bottts-neutral/svg?seed=${encodeURIComponent(login)}`;
 
-    const stmt = db.prepare('INSERT INTO users (login, email, avatarUrl, auth_provider) VALUES (?, ?, ?, ?)');
-    stmt.run(login, email, avatarUrl, 'google');
+    const stmt = db.prepare(
+      "INSERT INTO users (login, email, avatarUrl, auth_provider) VALUES (?, ?, ?, ?)",
+    );
+    stmt.run(login, email, avatarUrl, "google");
 
     reply.code(201).send({ login, email });
   } catch (err) {
-    console.error('Google sign-up error:', err);
-    reply.status(401).send({ error: 'Invalid Google token' });
+    console.error("Google sign-up error:", err);
+    reply.status(401).send({ error: "Invalid Google token" });
   }
 }
 
@@ -113,7 +128,9 @@ export async function signIn(req, reply) {
 
   try {
     if (!givenLogin || !password) {
-      return reply.status(400).send({ error: 'Login and password are required' });
+      return reply
+        .status(400)
+        .send({ error: "Login and password are required" });
     }
 
     givenLogin = givenLogin.trim();
@@ -123,22 +140,22 @@ export async function signIn(req, reply) {
       WHERE login = ? OR email = ?
       LIMIT 1
     `);
-    const user = stmt.get(givenLogin, givenLogin);  // cherche par login ou email
+    const user = stmt.get(givenLogin, givenLogin); // cherche par login ou email
 
     if (!user) {
-      return reply.status(401).send({ error: 'Invalid login or password' });
+      return reply.status(401).send({ error: "Invalid login or password" });
     }
 
     // askip bcrypt.compare(password, user.password) c est mieux mais jsp
     if (password !== user.password) {
-      return reply.status(401).send({ error: 'Invalid password' });
+      return reply.status(401).send({ error: "Invalid password" });
     }
 
     // JWT pour user
-    const token = await reply.jwtSign({ 
-      login: user.login, 
-      email: user.email, 
-      avatarUrl: user.avatarUrl 
+    const token = await reply.jwtSign({
+      login: user.login,
+      email: user.email,
+      avatarUrl: user.avatarUrl,
     });
 
     return reply.code(200).send({
@@ -146,9 +163,8 @@ export async function signIn(req, reply) {
       login: user.login,
       email: user.email,
     });
-  } 
-  catch (err) {
-    console.error('Sign-in error:', err);
-    return reply.status(500).send({ error: 'Internal server error' });
+  } catch (err) {
+    console.error("Sign-in error:", err);
+    return reply.status(500).send({ error: "Internal server error" });
   }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,51 +1,64 @@
-import fastify from 'fastify'
-import path from 'path'
-import fastifyStatic from '@fastify/static'
-import { fileURLToPath } from 'url'
-import db from './db.js'
-import userRoutes from './routes/user.js'
-import matchRoutes from './routes/matches.js'
-import formbody from '@fastify/formbody'
-import cors from '@fastify/cors'
-import authRoutes from './routes/auth.js'
-import jwt from '@fastify/jwt'
-import dotenv from 'dotenv'
+import fastify from "fastify";
+import path from "path";
+import fastifyStatic from "@fastify/static";
+import { fileURLToPath } from "url";
+import db from "./db.js";
+import userRoutes from "./routes/user.js";
+import matchRoutes from "./routes/matches.js";
+import formbody from "@fastify/formbody";
+import cors from "@fastify/cors";
+import authRoutes from "./routes/auth.js";
+import jwt from "@fastify/jwt";
+import dotenv from "dotenv";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 
-const app = fastify()
+const app = fastify();
 
 await app.register(cors, {
-  origin : true
-})
+  origin: true,
+});
 
-console.log('JWT secret:', process.env.JWT_SECRET)
+console.log("JWT secret:", process.env.JWT_SECRET);
 
-
-app.register(jwt,  {
-  secret: process.env.JWT_SECRET
-})
+app.register(jwt, {
+  secret: process.env.JWT_SECRET,
+});
 
 app.register(fastifyStatic, {
-  root: path.join(__dirname, '../frontend'),
-  prefix: '/'
-})
+  root: path.join(__dirname, "../frontend"),
+  prefix: "/",
+});
 
+app.register(formbody);
+app.register(userRoutes);
+app.register(matchRoutes);
+app.register(authRoutes);
 
-app.register(formbody)
-app.register(userRoutes)
-app.register(matchRoutes)
-app.register(authRoutes)
+// pour SPA sinon les routes pas trouve
+const spaRoutes = [
+  "/",
+  "/game",
+  "/auth",
+  "/game/tournament",
+  "/game/dashboard",
+  "/game/profile",
+];
 
+spaRoutes.forEach((route) => {
+  app.get(route, async (request, reply) => {
+    return reply.sendFile("index.html");
+  });
+});
+// fin de ce que jai rajoute + pour linstant refresh = deco pcq pas de jwt
 const start = async () => {
   try {
-    await app.listen({ port: 8000, host: '0.0.0.0' })
+    await app.listen({ port: 8000, host: "0.0.0.0" });
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-}
-start()
-
+};
+start();
